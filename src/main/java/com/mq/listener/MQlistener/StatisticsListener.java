a7eecde0f1a1038cce00bcb494172fe4005fb785
Fergus2299 <fergusjcassidy@gmail.com>
2023-08-18 18:11:54 +0100

    puts and gets improvements

--- src/main/java/com/mq/listener/MQlistener/StatisticsListener.java
+++ src/main/java/com/mq/listener/MQlistener/StatisticsListener.java
@@ -6,12 +6,16 @@ import jakarta.jms.JMSException;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.Iterator;
 
 import org.slf4j.Logger;
@@ -34,9 +38,9 @@ public class StatisticsListener {
     // storing a hashmap for each queue
     // outer map stores queue name
     // inner map has puts, gets,...
+    private static final Set<String> OBSERVED_QUEUES = new HashSet<>();
     private static final Map<String, Map<String, Integer>> queueStatsMap = new HashMap<>();
     private static final Map<String, Map<String, Map<String, Integer>>> timeSeriesStats = new LinkedHashMap<>();
-    private static int periodCounter = 0;
     @JmsListener(destination = "SYSTEM.ADMIN.STATISTICS.QUEUE")
     public void listen(Message receivedMessage) throws JMSException {
         if (receivedMessage instanceof BytesMessage) {
@@ -44,39 +48,36 @@ public class StatisticsListener {
             try {
                 MQMessage mqMsg = MQListener.convertToMQMessage((BytesMessage) receivedMessage);
                 PCFMessage pcfMsg = new PCFMessage(mqMsg);
-//                System.out.println(pcfMsg);
                 processStatQMessage(pcfMsg);
             } catch (Exception e) {
                 MQListener.logProcessingError(e, "PCF");
-                
             }
         } else {
             log.warn("Received non-bytes message: {}", receivedMessage);
         }
     }
-    private static void processStatQMessage(PCFMessage pcfMsg) {
+    private void processStatQMessage(PCFMessage pcfMsg) {
         if (pcfMsg == null) {
             log.error("Provided PCFMessage is null.");
             return;
         }
         try {
         	// get date/time
-        	String startDate = pcfMsg.getStringParameterValue(MQConstants.MQCAMO_START_DATE).trim();
-        	String startTime = pcfMsg.getStringParameterValue(MQConstants.MQCAMO_START_TIME).trim();
-        	String endDate = pcfMsg.getStringParameterValue(MQConstants.MQCAMO_END_DATE).trim();
-        	String endTime = pcfMsg.getStringParameterValue(MQConstants.MQCAMO_END_TIME).trim();
+//        	String startDate = pcfMsg.getStringParameterValue(MQConstants.MQCAMO_START_DATE).trim();
+//        	String startTime = pcfMsg.getStringParameterValue(MQConstants.MQCAMO_START_TIME).trim();
+//        	String endDate = pcfMsg.getStringParameterValue(MQConstants.MQCAMO_END_DATE).trim();
+//        	String endTime = pcfMsg.getStringParameterValue(MQConstants.MQCAMO_END_TIME).trim();
+        	String currentDateTime = LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME);     
         	
         	Enumeration<?> parameters = pcfMsg.getParameters();
         	while (parameters.hasMoreElements()) {
 	            PCFParameter parameter = (PCFParameter) parameters.nextElement();
-	            
 	            // for now assuming that MQCFGR is always a queue
 	            // TODO: check that this is always a queue and in the same format
 	            if (parameter instanceof MQCFGR) {
 	                MQCFGR fgr = (MQCFGR) parameter;
 	                Enumeration<?> nestedParameters = fgr.getParameters();
 	                // loop until there's no more elems in the Group 
-	                
 	                // for each queue we want the amount put and got 
 	                String QName = null;
 	                Integer quantPut = 0;
@@ -89,14 +90,9 @@ public class StatisticsListener {
 	                    switch (nestedParameter.getParameter()) {
 		                    case MQConstants.MQCA_Q_NAME:
 		                        QName = nestedParameter.getStringValue();
-//		                        System.out.println("Queue Name: " + QName);
 		                        break;
+		                    // for now assuming puts and put1s are the same
 		                    case MQConstants.MQIAMO_PUTS:
-		                        if (value instanceof int[]) {
-		                            int firstElement = ((int[]) value)[0];
-		                            quantPut = quantPut + firstElement;
-		                        }
-		                        break;
 		                    case MQConstants.MQIAMO_PUT1S:
 		                        if (value instanceof int[]) {
 		                            int firstElement = ((int[]) value)[0];
@@ -104,11 +100,6 @@ public class StatisticsListener {
 		                        }
 	                            break;
 		                    case MQConstants.MQIAMO_PUTS_FAILED:
-		                    	if (value instanceof Integer) {
-		                            Integer failedValue = (Integer) value;
-		                            quantPutFails = quantPutFails + failedValue;
-		                        }
-	                            break;
 		                    case MQConstants.MQIAMO_PUT1S_FAILED:
 		                    	if (value instanceof Integer) {
 		                            Integer failedValue = (Integer) value;
@@ -128,42 +119,52 @@ public class StatisticsListener {
 		                        }
 	                            break;
                     	}
-	                    
                 	}
-	                
-	                // rudimentary removal of admin queues
+	                // adding the queue's stats to the queue stats map for this period - always a new map
 	                if(QName != null && !QName.contains("ADMIN") && !QName.contains("SYSTEM")) {
-	                    Map<String, Integer> statsForQueue = queueStatsMap.getOrDefault(QName, new HashMap<>());
+	                    OBSERVED_QUEUES.add(QName);
+	                    Map<String, Integer> statsForQueue = new HashMap<>();
 	                    statsForQueue.put("PUTS", quantPut);
 	                    statsForQueue.put("PUTS_FAILED", quantPutFails);
 	                    statsForQueue.put("GETS", quantGet);
 	                    statsForQueue.put("GETS_FAILED", quantGetFails);
-
 	                    queueStatsMap.put(QName, statsForQueue);
-	                	String timeKey = startDate + startTime + " - " + endDate + ":" + endTime;
-		                timeSeriesStats.put(timeKey, new HashMap<>(queueStatsMap));      
-		                ensureMaxEntries();
-		                printTimeSeriesStats();
 	                }
-
 	            }
     		}
+//        	System.out.println("Initial stats queue");
+//        	printQueueStatsMap();
+        	printObservedQueues();
+            for (String queue : OBSERVED_QUEUES) {
+                if (!queueStatsMap.containsKey(queue)) {
+                    Map<String, Integer> defaultStats = new HashMap<>();
+                    defaultStats.put("PUTS", 0);
+                    defaultStats.put("PUTS_FAILED", 0);
+                    defaultStats.put("GETS", 0);
+                    defaultStats.put("GETS_FAILED", 0);
+                    queueStatsMap.put(queue, defaultStats);
+                }
+            }
+            System.out.println("new stats queue");
+//            printQueueStatsMap();
+        	
+            
+            
+	        timeSeriesStats.put(currentDateTime, new HashMap<>(queueStatsMap));      
+	        ensureMaxEntries();
+	        printTimeSeriesStats();
+            queueStatsMap.clear();
+        	
         } catch (RuntimeException e) {
             System.out.println("Error occurred while parsing PCFMessage: " + e.getMessage());
             e.printStackTrace();
             log.error("Error occurred while parsing PCFMessage: " + e.getMessage(), e);
-        } catch (PCFException e) {
-        	System.out.println("Error occurred while parsing PCFMessage: " + e);
-        }
-        periodCounter++;
-        if (periodCounter >= 2) {
-            generateCSVForAllQueues();
-            periodCounter = 0;
         }
 
-        	
-
+        
+        
     }
+    
     private static void printTimeSeriesStats() {
         timeSeriesStats.forEach((time, stats) -> {
             System.out.println("Time: " + time);
@@ -175,9 +176,13 @@ public class StatisticsListener {
             });
         });
     }
+    
+    /**
+    When the size of timeSeriesStats gets to 30, this deletes the oldest entry, 
+    to ensure not too much memory usage.
+    */
     private static void ensureMaxEntries() {
         while (timeSeriesStats.size() > 30) {
-            // Remove the oldest entry (which is the first one)
             Iterator<String> it = timeSeriesStats.keySet().iterator();
             if (it.hasNext()) {
                 it.next();
@@ -185,24 +190,25 @@ public class StatisticsListener {
             }
         }
     }
-    private static void generateCSVForAllQueues() {
-    	System.out.println("generating report");
-        timeSeriesStats.forEach((time, stats) -> {
-            stats.forEach((queueName, queueStats) -> {
-                String csvFileName = queueName + ".csv";
-                try (FileWriter writer = new FileWriter(csvFileName, true)) {
-                    writer.append(time);
-                    for (String metric : Arrays.asList("PUTS", "PUTS_FAILED", "GETS", "GETS_FAILED")) {
-                        writer.append(',');
-                        writer.append(String.valueOf(queueStats.getOrDefault(metric, 0)));
-                    }
-                    writer.append('\n');
-                } catch (IOException e) {
-                    log.error("Error occurred while writing to CSV file: " + e.getMessage(), e);
-                }
+    
+    
+    
+    
+    private static void printQueueStatsMap() {
+        System.out.println("======== Queue Statistics Map ========");
+        queueStatsMap.forEach((queueName, stats) -> {
+            System.out.println("Queue Name: " + queueName);
+            stats.forEach((key, value) -> {
+                System.out.println("\t" + key + ": " + value);
             });
+            System.out.println("====================================");
         });
     }
-
-  
+    private static void printObservedQueues() {
+        System.out.println("======== Observed Queues ========");
+        for (String queueName : OBSERVED_QUEUES) {
+            System.out.println(queueName);
+        }
+        System.out.println("=================================");
+    }
 }
