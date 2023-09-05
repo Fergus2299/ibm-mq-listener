c90bcbc923aae60cbb4f17d2592cea3697d8c8e7
Fergus2299 <fergusjcassidy@gmail.com>
2023-09-06 00:21:26 +0100

    changes to python file

--- startApp.py
+++ startApp.py
@@ -1,5 +1,5 @@
 import subprocess
-
+import os
 '''
 This is a python file which will run the Java for you.
 
@@ -11,6 +11,45 @@ mvn spring-boot:run
 def start_spring_boot_app():
     cmd = ["mvn", "spring-boot:run"]
     subprocess.run(cmd)
+
+def set_env_variables(**properties):
+    """
+    Set environment variables from provided properties.
+    """
+    for key, value in properties.items():
+        os.environ[key] = value
+
+def start_spring_boot_app():
+    """
+    Start the Spring Boot application.
+    """
+    cmd = ["mvn", "spring-boot:run"]
+    subprocess.run(cmd)
+    
+    
+    
+def run_with_properties(queue_manager, channel, conn_name, user, password, listener_auto_startup):
+    """
+    Accepts properties as arguments, sets them as environment variables, 
+    and starts the Spring Boot application.
+    """
+    set_env_variables(
+        ibm_mq_queueManager=queue_manager,
+        ibm_mq_channel=channel,
+        ibm_mq_connName=conn_name,
+        ibm_mq_user=user,
+        ibm_mq_password=password,
+        spring_jms_listener_auto_startup=listener_auto_startup
+    )
+    start_spring_boot_app()    
+    
     
 if __name__ == "__main__":
-    start_spring_boot_app()
+        run_with_properties(
+        queue_manager="QM1",
+        channel="DEV.ADMIN.SVRCONN",
+        conn_name="13.87.80.195(1414)",
+        user="admin",
+        password="passw0rd",
+        listener_auto_startup="false"
+    )
