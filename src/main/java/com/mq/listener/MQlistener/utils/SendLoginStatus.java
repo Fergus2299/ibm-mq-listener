4bbf64823ca18d297f72fb333319b4f2c11fe1c5
Fergus2299 <fergusjcassidy@gmail.com>
2023-09-07 16:24:49 +0100

    login status sender

--- src/main/java/com/mq/listener/MQlistener/utils/SendLoginStatus.java
+++ src/main/java/com/mq/listener/MQlistener/utils/SendLoginStatus.java
@@ -1,46 +1,45 @@
 package com.mq.listener.MQlistener.utils;
 
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Collections;
-import java.util.Map;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+
+import reactor.core.publisher.Mono;
 
 @Service
 public class SendLoginStatus {
-	// TODO: change to web client
-	private final String POST_URL = "https://127.0.0.1:5000/javaloginfeedback";
-	
+    private final String POST_URL = "https://127.0.0.1:5000/javaloginfeedback";
+    private final WebClient webClient;
+
+    @Autowired
+    public SendLoginStatus(WebClient.Builder webClientBuilder) {
+        this.webClient = webClientBuilder.baseUrl(POST_URL).build();
+    }
+
     public void sendStatus(Boolean success, String message) {
-        RestTemplate restTemplate = new RestTemplate();
-        Map<String, String> jsonPayload = Collections.singletonMap("message", message);
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("message", message);
 
-        HttpEntity<Map<String, String>> request = new HttpEntity<>(jsonPayload, headers);
-
-        try {
-            ResponseEntity<String> response = restTemplate.postForEntity(POST_URL, request, String.class);
-            if (response.getStatusCode().is2xxSuccessful()) {
-                System.out.println("POST request successful. Response: " + response.getBody());
-            } else {
-                System.out.println("POST request returned an unexpected status: " + response.getStatusCode());
-            }
-        } catch (HttpClientErrorException e) {
-            // Specific exception for 4xx errors
-            System.err.println("Error " + e.getStatusCode() + ": " + e.getResponseBodyAsString());
-        } catch (RestClientException e) {
-            // Base exception for RestTemplate, catches general network issues
-            System.err.println("Error sending POST request: " + e.getMessage());
-        } catch (Exception e) {
-            // Handle other exceptions that may occur
-            System.err.println("An unexpected error occurred: " + e.getMessage());
-        }
-    } 
+        webClient.post()
+                .uri(POST_URL)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(jsonObject.toString()), String.class)
+                .retrieve()
+                .bodyToMono(String.class)
+                .subscribe(
+                        response -> System.out.println("POST request successful. Response: " + response),
+                        error -> {
+                            if (error instanceof WebClientResponseException) {
+                                WebClientResponseException webClientError = (WebClientResponseException) error;
+                                System.err.println("Error " + webClientError.getStatusCode() + ": " + webClientError.getResponseBodyAsString());
+                            } else {
+                                System.err.println("Error sending POST request: " + error.getMessage());
+                            }
+                        }
+                );
+    }
 }
